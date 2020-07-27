6348601cc8517e86c1d67b2f5513fad363ef6bc8
JamieVarthan <60700248+JamieVarthan@users.noreply.github.com>
2020-07-27 11:54:32 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,7 +17,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned 
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     
-    req = requests.get("https://telegra.ph/file/0670190de8e3bddea6d95.png")
+    req = requests.get("https://telegra.ph/file/f58aa88c51c6f7473eb12.jpg")
     req.raise_for_status()
     file = BytesIO(req.content)
     file.seek(0)
@@ -26,13 +26,13 @@ async def amireallyalive(alive):
         img.save(sticker, "webp")
         sticker.name = "sticker.webp"
         sticker.seek(0)
-        await borg.send_message(alive.chat_id, f"**Welcome To TeleBot **\n\n"
-            "**`Hey! I'm Spade Security System !!. All systems online and functioning normally!`**\n\n"
-            "` 🔸 Telethon version:` **1.15.0**\n` 🔹 Python:` **3.8.3**\n"
-            "` 🔸 More info:` No More INFO , Write your Own Script!!\n"
-            "` 🔹 Bot created by:` [Spade](https://t.me/spade_csoadmin)\n"
-            "` 🔸 Database Status:` **Nothing is Running!**\n"
-            f"` 🔹 My pro owner`: {DEFAULTUSER}\n\n"
-            "    [✨ Private Bot Service , If needed contact My Owner ✨](https://t.me/spade_csoadmim)", link_preview = False)
+        await borg.send_message(alive.chat_id, f"**Welcome To Spade User Bot **\n\n"
+            "**`Hey! I'm Spade Security System !!.`**\n\n"
+            "` 🔸 Userbot version:` **1.0**\n` 🔹 Python:` **3.5**\n"
+            "` 🔸 More info:` No More INFO , Write your Own Python Code!!\n"
+            "` 🔹 Bot created by:` [Spade](https://t.me/wolverine_smash)\n"
+            "` 🔸 Database Status:` **My name is Athena!**\n"
+            f"` 🔹 My Rightful owner`:  [Athena](https://t.me/athena_the_warrior)\n\n"
+            "    [✨ Private Bot Service , If needed contact My Owner ✨](https://t.me/wolverine_smash)", link_preview = False)
         await borg.send_file(alive.chat_id, file=sticker) 
         await alive.delete()
